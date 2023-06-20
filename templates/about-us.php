463b600d21b31e2e7e479b29cae8129c8fe1103e
trickophp <n.tricko2003@gmail.com>
2023-06-20 15:36:59 +0200

    about us

---
+++ templates/about-us.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: O nama Template */
+get_header(); ?>
+
+<div class="about-hero">
+    <div class="about-hero-overlay"></div>
+    <h1>O NAMA</h1>
+</div>
+
+<div class="about-us-container">
+    <div class="about-us-copy">
+        <img src="/wp-content/uploads/2023/06/pexels-李昂軒-3877818-scaled.jpg" alt="">
+        <div>
+            <h2>Klasek pirotehnika</h2>
+            <p>Firma Klasek Trading osnovana je 1990. godine, a zahvaljujući dugogodišnjem iskustvu i visokom kvalitetu proizvoda postala je vodeći proizvođač i uvoznik amaterske i profesionalne pirotehnike koju distribuira u preko 25 zemalja Evrope. Klasek pirotehnika dostupna je na Srpskom tržištu od 2021 godine</p>
+        </div>
+    </div>
+    <div class="about-us-copy-reverse">
+        <img src="/wp-content/uploads/2023/06/pexels-prakhar-bansal-732183-scaled.jpg" alt="">
+        <div>
+            <h2>Kvalitet</h2>
+            <p>Klasek pirotehnika kao prioritet postavlja kvalitet, cenu, sigurnost i pouzdanost. Kvalitet nadmašuje standarde u odnosu na cene proizvoda istih kategorija drugih proizvođača. Svaki kupac dobija maksimum za svoj novac a to su jačina, kvalitetan i čist efekat.</p>
+        </div>
+    </div>
+    <div class="about-us-copy">
+        <img src="/wp-content/uploads/2023/06/pexels-sarah-trummer-955792-scaled.jpg" alt="">
+        <div>
+            <h2>Sigurnost</h2>
+            <p>Klasek pirotehnika izrađena je u skladu sa Evropskim direktivama i poseduje CE sertifikate. Za sve artikle zvršena je usaglašenost sa Evropskim standardima a po Srpskim standardima koji su još strožiji od Evropskih. Sva roba poseduje Rešenje MUP-a o odobrenom stavljanju u promet na teritoriji Republike Srbije. Jedino mi imamo odobrenje za stavljanje u promet po Evropskoj klasifikaciji proizvoda (kategorije F1, F2, F3, F4, P1, P2, T1 i T2) tako da spremno dočekujemo primenu novog Zakona usklađenog sa Evropskim.</p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
