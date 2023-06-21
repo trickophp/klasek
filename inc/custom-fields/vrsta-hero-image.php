62a6d9ae3bf6fedfc1442f944a60dc1a49a0b970
satrain <mladen.trickovic5@gmail.com>
2023-06-21 21:25:51 +0200

    update acf fields and load them from the php

---
+++ inc/custom-fields/vrsta-hero-image.php
@@ -0,0 +1,57 @@
+<?php
+
+if ( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_648f1b6a08d3d',
+        'title' => 'Vrsta term fields',
+        'fields' => array(
+            array(
+                'key' => 'field_648f1b6b9bbf7',
+                'label' => 'Hero section image',
+                'name' => 'hero_section_image',
+                'aria-label' => '',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'url',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+                'preview_size' => 'medium',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'taxonomy',
+                    'operator' => '==',
+                    'value' => 'vrsta',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
+    
+endif;
+
+?>
--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -2,7 +2,7 @@
     <div class="fo-product-card">
         <p class="fo-product-card-title"><?php the_title() ?></p>
         <?php the_post_thumbnail(); ?>
-        <p class="fo-product-description"><?= fo_get_product_excerpt($post->ID) ?></p>
+        <?php /* <p class="fo-product-description"><?= fo_get_product_excerpt($post->ID) ?></p> */ ?>
         <span class="fo-product-serial-number">Šifra: <?= get_field('sifra_proizvoda', $post->ID) ?></span>
     </div>
 </a>
